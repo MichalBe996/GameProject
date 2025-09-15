0cc934d2da241e5da82ec8aad1fb1942ba358c5d
Michał <michal.bielawski@hotmail.com>
2025-09-15 16:19:39 +0200

    Second Commit test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is main game file");
+        System.out.println("Second commit test");
 
         }
     }
