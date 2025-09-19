8af4c2d7eae80783276db49770d7b1abb633c699
Michał <michal.bielawski@hotmail.com>
2025-09-19 13:40:03 +0200

    Moving rectangle

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,16 +1,38 @@
 package main;
 
+import inputs.KeyboardInputs;
+
 import javax.swing.*;
 import java.awt.*;
 
+
 public class GamePanel extends JPanel {
+    private int xDelta = 0, yDelta = 0;
     public GamePanel() {
+        addKeyListener(new KeyboardInputs(this));
     }
+
+    public void checkBorders(){
+
+    }
+    public void changeXDelta(int value){
+        this.xDelta += value;
+
+        repaint();
+
+    }
+    public void changeYDelta(int value){
+        this.yDelta += value;
+        repaint();
+
+    }
+
     public void paintComponent(Graphics g){
         // calling super class paintComponent method
         super.paintComponent(g);
 
-        g.drawRect(100,100,200,50);
+        g.fillRect(100+xDelta, 100 + yDelta,200,50);
+
 
     }
 }
