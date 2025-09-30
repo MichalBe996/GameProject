90e16239a5df75c91a769050ae9e3e82a26e7786
Michał <michal.bielawski@hotmail.com>
2025-09-30 14:41:28 +0200

    Implementing gameloop

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,13 +1,37 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private boolean gameLoopRunning = true;
+    private final int FPS_SET = 120;
     public Game(){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
 
+
+    }
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 10000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        while (gameLoopRunning){
+            now = System.nanoTime();
+            if(now - lastFrame >= timePerFrame){
+                gamePanel.repaint();
+                lastFrame = now;
+            }
+
+        }
 
     }
 }
